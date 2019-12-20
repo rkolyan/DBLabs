62c2ab6133e65e3e5acc521ab5ba689527cd56bf
r_kolyan <blendervik@gmail.com>
2019-12-20 18:46:31 +0300

    Add 1 for lab_05

---
+++ lab_05/a2.sql
@@ -0,0 +1,11 @@
+--Здесь должен быть аналог for xml auto, path, explicit
+SELECT
+    XMLCONCAT('<!--Just an xmlresult-->', E'\n', XMLFOREST(tb1."xml-val" as "Genres"), E'\n') AS xmlresult
+FROM
+    (
+        SELECT
+           XMLAGG(XMLELEMENT(NAME genre, XMLATTRIBUTES(G.genre_id AS id, G.name as name), E'\n')) AS
+            "xml-val"
+        FROM
+            lab_01.genres AS G
+    ) AS tb1;
