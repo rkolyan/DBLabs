13c51876ec38f06f7130439a52f02bf91a06f781
r_kolyan <blendervik@gmail.com>
2020-01-06 21:12:06 +0300

    Add python 3 xmlparsing

--- lab_06/xmlparse.py
+++ lab_06/xmlparse.py
@@ -6,9 +6,9 @@ def parseXML(xmlFile):
     Парсинг XML
     """
     with open(xmlFile) as fobj:
-        xml = fobj.read()
+        xml = fobj.read();
     
-    root = etree.fromstring(xml)
+    root = etree.fromstring(xml);
     
     for appt in root.getchildren():
         for elem in appt.getchildren():
@@ -17,8 +17,26 @@ def parseXML(xmlFile):
             else:
                 text = elem.text
             
-            print(elem.tag + " => " + text)
+            print(elem.tag + " => " + text);
+        print("\n");
+    return root;
 
+def updateXML(root):
+    new_element = etree.SubElement(root, "musicians");
+    etree.SubElement(new_element, "musicians_id").text = "34";
+    etree.SubElement(new_element, "name").text = "SuperSayan";
+    etree.SubElement(new_element, "people_count").text = "12";
+    etree.SubElement(new_element, "birthday").text = "1992";
+    etree.SubElement(new_element, "is_dead").text = "1";
+    etree.SubElement(new_element, "country").text = "Japan";
+    return root;
+
+def saveXML(root):
+    f = open('b1-output.xml', 'wb');
+    f.write(etree.tostring(root, pretty_print=True, xml_declaration=True));
+    f.close();
 
 if __name__ == "__main__":
-    parseXML("example.xml")
+    root = parseXML("b1-input.xml");
+    root = updateXML(root);
+    saveXML(root);
