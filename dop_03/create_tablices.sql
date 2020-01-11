b1a86c48cd1f719035cd001eae111859dc3d1269
r_kolyan <blendervik@gmail.com>
2020-01-11 12:29:43 +0300

    Add dop_4

---
+++ dop_03/create_tablices.sql
@@ -0,0 +1,51 @@
+DROP TABLE IF EXISTS persons CASCADE;
+DROP TABLE IF EXISTS holiday_types CASCADE;
+DROP TABLE IF EXISTS holidays CASCADE;
+DROP TABLE IF EXISTS persons CASCADE;
+
+CREATE TABLE persons
+(
+	id INT NOT NULL PRIMARY KEY,
+	FIO VARCHAR(200)
+);
+
+INSERT INTO persons
+VALUES (1, 'Головнев Николай Васильевич'),
+(2, 'Стропонькин Василий Степанович'),
+(3, 'Иванов Петр Сидорович'),
+(4, 'Калым Ыж Ныр');
+
+CREATE TABLE holiday_types
+(
+	id INT NOT NULL PRIMARY KEY,
+	htype VARCHAR(200)
+);
+
+INSERT INTO holiday_types
+VALUES (1, 'За свой счет'),
+(2, 'Отпуск'),
+(3, 'Больничный');
+
+CREATE TABLE holidays
+(
+	id INT NOT NULL PRIMARY KEY,
+	pid INT NOT NULL REFERENCES persons(id),
+	hdate date,
+	hid INT NOT NULL REFERENCES holiday_types(id)
+);
+
+INSERT INTO holidays
+VALUES
+(1, 1, '20-01-2020', 1),
+(2, 1, '21-01-2020', 1),
+(3, 1, '22-01-2020', 1),
+(4, 1, '23-01-2020', 1),
+(5, 1, '25-01-2020', 3),
+(6, 1, '26-01-2020', 3),
+(7, 1, '02-02-2020', 1),
+(8, 2, '04-02-2020', 2),
+(9, 2, '05-02-2020', 2),
+(10, 2, '06-02-2020', 2),
+(11, 2, '08-04-2020', 3),
+(12, 3, '11-01-2020', 1);
+
---
+++ dop_03/query.sql
@@ -0,0 +1,47 @@
+TABLE persons;
+TABLE holiday_types;
+TABLE holidays;
+
+--Сам запрос(нужны данные)
+SELECT row_number() over (order by T5.begin_data),
+P.fio, T5.begin_data, T5.end_data, HT.htype
+FROM
+(
+	SELECT * 
+	FROM
+	(
+		SELECT pid, hid, --Определяем первое число и последнее число
+		CASE 
+			WHEN previous IS NULL THEN hdate
+			ELSE next
+		END AS begin_data,
+		CASE
+			WHEN previous IS NULL and next2 IS NULL then hdate
+			ELSE next2
+		END AS end_data
+		FROM 
+		(
+				SELECT *, --Добавляем новую следующую дату, (next2)
+				lead(hdate) over(partition by pid, hid order by hdate) as next2
+				FROM
+				(
+					SELECT * --Оставляем только где есть null и разность дней > 1
+					FROM
+					(
+						SELECT pid, hid, hdate, --Находим для каждого числа следующее и предыдущее
+						lead(hdate) over (partition by pid, hid order by hdate) as next,
+						lag(hdate) over (partition by pid, hid order by hdate) as previous
+						FROM holidays
+						GROUP BY pid, hid, hdate
+					) AS T1
+					GROUP BY pid, hid, hdate, next, previous
+					HAVING next - hdate > 1 OR next IS NULL OR previous IS NULL
+				) AS T2
+				GROUP BY pid, hid, hdate, next, previous
+		) AS T3
+	) AS T4
+	WHERE begin_data IS NOT NULL
+) AS T5
+JOIN holiday_types AS HT ON T5.hid=HT.id
+JOIN persons AS P ON T5.pid=P.id
+ORDER BY begin_data;
